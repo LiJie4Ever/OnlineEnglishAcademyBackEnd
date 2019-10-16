082cb6024938c513c2b928798fec655f48686b03
David Xuan <davidxuanzc@gmail.com>
2019-10-16 16:22:27 -0700

    config.js

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDWNQ8jyh6xhAlx7vGo14FfQXvv4YUufrc",
+    authDomain: "onlineenglishacademy-eddb3.firebaseapp.com",
+    databaseURL: "https://onlineenglishacademy-eddb3.firebaseio.com",
+    projectId: "onlineenglishacademy-eddb3",
+    storageBucket: "onlineenglishacademy-eddb3.appspot.com",
+    messagingSenderId: "323155922070",
+    appId: "1:323155922070:web:6d60b51b6d9bbdb04a86b7",
+    measurementId: "G-Y85RDXB90Y"
+  };
