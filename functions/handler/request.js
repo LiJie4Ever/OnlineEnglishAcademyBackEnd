f35c01cf7fda7bc8d634f6780a79859858ceabb2
horace <horace_kim@outlook.com>
2019-11-19 12:17:19 -0800

    fic

--- functions/handler/request.js
+++ functions/handler/request.js
@@ -1,3 +1,5 @@
+//export GOOGLE_APPLICATION_CREDENTIALS=/home/horace/aela/OnlineEnglishAcademyBackEnd/functions/service-account-file.json
+
 const admin = require('firebase-admin');
 
 
@@ -7,12 +9,12 @@ exports.getRequestList = (req, res) => {
     let query = db.get()
         .then(snapshot => {
             snapshot.forEach(doc =>{
-                let reqObject = {student:doc.data().student, availableTime:doc.data().availableTime, status:doc.data().status,
+                let reqObject = {id:doc.id, student:doc.data().student, availableTime:doc.data().availableTime, status:doc.data().status,
                     numOfS:doc.data().numOfS, note:doc.data().note, preferredT1:doc.data().preferredT1, email:doc.data().email,
                     preferredT2:doc.data().preferredT2, preferredT3:doc.data().preferredT3, price:doc.data().price, timezone:doc.data().timezone};
                 list.push(reqObject);
             });
-            return res.status(200).json({ content: list});
+            return res.status(200).json({content: list});
         })
         .catch(err => {
             console.log('Error getting documents', err);
@@ -20,6 +22,7 @@ exports.getRequestList = (req, res) => {
         });
 };
 
+// no use
 exports.createRequest = (req, res) => {
     //console.log(JSON.stringify(req.body));
     //let rdata = JSON.parse(JSON.stringify(req.body));
@@ -59,7 +62,7 @@ exports.confirmRequest =(req, res) => {
             admin.firestore().collection('request').doc(req.body.id).update({
                 status:"1",
             });
-            res.status(200).json({"Success": "Price modified"});
+            res.status(200).json({"Success": "Request comfirmed"});
         } else {
             res.status(404).json({"Error": "Request not found"});
         }
@@ -89,7 +92,7 @@ exports.setRequestStatus = (req, res) => {
     .then(function(doc) {
         if (doc.exists) {
             admin.firestore().collection('request').doc(req.body.id).update({
-                status:"2",
+                status:2,
             });
             res.status(200).json({"Success": "Set request status successfully to paid"});
         } else {
