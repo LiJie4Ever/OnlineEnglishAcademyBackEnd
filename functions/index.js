8243f26f0e748b6c0ffd53186648b20635d4ff10
horace <horace_kim@outlook.com>
2019-11-18 18:13:38 -0800

    request api defined

--- functions/index.js
+++ functions/index.js
@@ -10,11 +10,24 @@ const {retrieveBlog} = require('./handler/blog');
 const {getCourses} = require('./handler/course');
 const {retrieveTeacherInfo} = require('./handler/teacher');
 
+//request related
+const {getRequestList} = require('./handler/request')
+const {createRequest} = require('./handler/request')
+const {setRequestPrice} = require('./handler/request')
+const {confirmRequest} = require('./handler/request')
+const {cancelRequest} = require('./handler/request')
+const {setRequestStatus} = require('./handler/request')
 //app.post('/create_meeting', signUp);
 
 app.get('/blog', retrieveBlog);
-app.get('/course', getCourses);
+//app.get('/course', getCourses);
 app.get('/teacher', retrieveTeacherInfo);
-
+//request related
+app.get('/request/getList', getRequestList);
+app.post('/request/create', createRequest);
+app.post('/request/setPrice', setRequestPrice);
+app.post('/request/confirm', confirmRequest);
+app.post('/request/cancel', cancelRequest);
+app.post('/request/setStatus', setRequestStatus);
 
 exports.api = functions.https.onRequest(app);
